d6bfb394332ba14683cb36c5a05617a170243ac1
Hidai Bar-Mor <hidai25@gmail.com>
2020-01-06 22:43:18 +0200

    Updates

---
+++ precache-manifest.87a0bb95c2200623c445b1702a3055f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd615fda7afa17eff946a625f4f3d430",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3831ddf11da699ff1a78",
+    "url": "/static/css/2.d48db53c.chunk.css"
+  },
+  {
+    "revision": "af6086721936a4e94b87",
+    "url": "/static/css/main.cd03d4f6.chunk.css"
+  },
+  {
+    "revision": "3831ddf11da699ff1a78",
+    "url": "/static/js/2.2541063b.chunk.js"
+  },
+  {
+    "revision": "af6086721936a4e94b87",
+    "url": "/static/js/main.c6b2e5ab.chunk.js"
+  },
+  {
+    "revision": "33c2271532df564d3d07",
+    "url": "/static/js/runtime-main.2edac438.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
