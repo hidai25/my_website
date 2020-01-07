7e094c02fc60cfa86d87b65d4f3940d92f19ac76
Hidai Bar-Mor <hidai25@gmail.com>
2020-01-07 22:35:55 +0200

    some changes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import BrowserRouter from 'react-router-dom/BrowserRouter'
-
+import Home from './components/Home';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 
 ReactDOM.render((
   <BrowserRouter basename={window.location.pathname || ''}>
-  <App />
+  // <App />
+  <Route exact path="/"  component={Home} />
 </BrowserRouter>
 ), document.getElementById('root'))
 
