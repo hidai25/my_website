4de51b9bdb581bf85d844095deaf49f71059019a
Hidai Bar-Mor <hidai25@gmail.com>
2020-01-12 12:30:41 +0200

    some work

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import BrowserRouter from 'react-router-dom/BrowserRouter'
+import { BrowserRouter} from 'react-router-dom';
 import {  Switch, Route } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
