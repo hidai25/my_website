c884781b77a44d27876b3b2213855c42a9005aa8
Hidai Bar-Mor <hidai25@gmail.com>
2020-01-12 12:31:12 +0200

    Updates

---
+++ precache-manifest.ea227192e6b69f449bff933586b4eda8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7fd276d8493d7351fd24bd49e9236e9",
+    "url": "/my_website/index.html"
+  },
+  {
+    "revision": "fc1eaa15f142c3c67fb9",
+    "url": "/my_website/static/css/2.9b6d80e4.chunk.css"
+  },
+  {
+    "revision": "0a927c541b4bd5522d6a",
+    "url": "/my_website/static/css/main.cd03d4f6.chunk.css"
+  },
+  {
+    "revision": "fc1eaa15f142c3c67fb9",
+    "url": "/my_website/static/js/2.4910421a.chunk.js"
+  },
+  {
+    "revision": "0a927c541b4bd5522d6a",
+    "url": "/my_website/static/js/main.008856f3.chunk.js"
+  },
+  {
+    "revision": "0c17bc9d767abbc1f347",
+    "url": "/my_website/static/js/runtime-main.bc2802bd.js"
+  }
+]);
