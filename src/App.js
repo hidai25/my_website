c6db223f59d3c8be902d5eadaa7e2e11da2e6228
Hidai Bar-Mor <hidai25@gmail.com>
2020-01-12 13:02:29 +0200

    some changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import {  Switch, Route } from 'react-router-dom';
 import {BrowserRouter} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import {Layout, Header, Navigation, Drawer, Content, download} from 'react-mdl';
 import Home from './components/Home';
 import About from './components/About.jsx';
 import Contact from './components/Contact.js';
@@ -29,3 +31,38 @@ function App() {
 }
 
 export default App;
+
+
+
+
+// import Main from './components/main';
+//
+//
+// function App() {
+//   return (
+//     <div className="demo-big-content">
+//     <Layout>
+//         <Header className="header-color" title="Title" scroll>
+//             <Navigation>
+//                 <a href="DeveloperResFINALReactNot.pdf" download>Resume</a>
+//                 <Link to="/projects">Projects</Link>
+//                 <Link to="/contact">Contact</Link>
+//             </Navigation>
+//         </Header>
+//         <Drawer className="nav-color" title="Home">
+//             <Navigation className="nav-color">
+//               <a href="DeveloperResFINALReactNot.pdf" download>Resume</a>
+//               <Link to="/projects">Projects</Link>
+//               <Link to="/contact">Contact</Link>
+//             </Navigation>
+//         </Drawer>
+//         <Content>
+//             <div className="page-content" />
+//             <Main/>
+//         </Content>
+//     </Layout>
+// </div>
+//   );
+// }
+//
+// export default App;
