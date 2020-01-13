2d3477b6b7d21fef8862e854aac3c07a3208965d
Hidai Bar-Mor <hidai25@gmail.com>
2020-01-13 21:53:29 +0200

    Updates

--- precache-manifest.6499c5870a36b0955abee0e40bc721e8.js
+++ precache-manifest.6499c5870a36b0955abee0e40bc721e8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7fcea2521428dd6e7eb5120e0cc4538b",
+    "revision": "0854a074963bf07a52efd5c7b73891c3",
     "url": "/my_website/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/my_website/static/css/2.4d0a10e7.chunk.css"
   },
   {
-    "revision": "27ce22f70b2b3d3b610f",
-    "url": "/my_website/static/css/main.cd03d4f6.chunk.css"
+    "revision": "cec44db3e8e7161675eb",
+    "url": "/my_website/static/css/main.2de64a9e.chunk.css"
   },
   {
     "revision": "bd6bad86c1c5d10a8819",
     "url": "/my_website/static/js/2.e5626476.chunk.js"
   },
   {
-    "revision": "27ce22f70b2b3d3b610f",
-    "url": "/my_website/static/js/main.b668ac08.chunk.js"
+    "revision": "cec44db3e8e7161675eb",
+    "url": "/my_website/static/js/main.1c428037.chunk.js"
   },
   {
     "revision": "0c17bc9d767abbc1f347",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my_website/precache-manifest.31167918c0b8242c2613ecc0ea7dc5fa.js"
+  "/my_website/precache-manifest.6499c5870a36b0955abee0e40bc721e8.js"
 );
 
 self.addEventListener('message', (event) => {
