d6bfe9bbd966e747a1d88237a485631a4cf52d96
Hidai Bar-Mor <hidai25@gmail.com>
2020-01-13 22:50:16 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my_website/precache-manifest.8de8bf1fef30bac7123af99dd5c638b9.js"
+  "/my_website/precache-manifest.30f5eea19cebf4e1ddce07c8df37435e.js"
 );
 
 self.addEventListener('message', (event) => {
