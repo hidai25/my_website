6a511a787ff0401cc117a1df860f7c2b06cc636d
Hidai Bar-Mor <hidai25@gmail.com>
2020-01-13 21:29:57 +0200

    some changes

--- src/App.js
+++ src/App.js
@@ -19,18 +19,18 @@ function App() {
 
    <div className="App">
    <Layout>
-           <Header className="header-color" title="Title" scroll>
-               <Navigation>
-                   <Link to="/projects">Projects</Link>
-                   <Link to="/contact">Contact</Link>
-               </Navigation>
-           </Header>
-           <Drawer className="nav-color" title="Home">
-               <Navigation className="nav-color">
-                 <Link to="/projects">Projects</Link>
-                 <Link to="/contact">Contact</Link>
-               </Navigation>
-           </Drawer>
+           // <Header className="header-color" title="Title" scroll>
+           //     <Navigation>
+           //         <Link to="/projects">Projects</Link>
+           //         <Link to="/contact">Contact</Link>
+           //     </Navigation>
+           // </Header>
+           // <Drawer className="nav-color" title="Home">
+           //     <Navigation className="nav-color">
+           //       <Link to="/projects">Projects</Link>
+           //       <Link to="/contact">Contact</Link>
+           //     </Navigation>
+           // </Drawer>
            <Content>
                // <div className="page-content" />
                <Main/>
