e5d69b80474288f86a235f36fe282e1d5e946f88
Hidai Bar-Mor <hidai25@gmail.com>
2020-01-19 10:51:27 +0200

    some changes

--- src/components/Academics_and_Skills.js
+++ src/components/Academics_and_Skills.js
@@ -29,36 +29,119 @@ class Home extends Component {
 
 
          <Navbar fixed="top" />
-          <h2>My skills</h2>
+          <br/>
 
-          <div class="w3-container">
-
-
-
-        <Progress multi>
-                <Progress bar value="30">JAVASCRIPT</Progress>
-                <div class="progress-bar">
-                   <span class="progress-value" >75%</span>
-                        </div>
-              </Progress><br/>
-
-
-        <Progress multi>
-                <Progress bar value="30">HTML</Progress>
-                <div class="progress-bar">
-                   <span class="progress-value yellow" >85%</span>
-                        </div>
-              </Progress><br/>
+<div class="plate">
+  <p class="script"><span>My Skills</span></p></div>
+<br/>
+<br/>
+<br/>
+          <div class="w3-cell-row">
+            <div class="w3-container w3-white w3-cell">
+              <p>I am a finance profession and experienced software developer</p>
+            </div>
+            <div class="w3-container w3-white w3-cell">
 
 
-              <Progress multi>
-                      <Progress bar value="30">CSS</Progress>
-                      <div class="progress-bar">
-                         <span class="progress-value green" >90%</span>
-                              </div>
-                    </Progress>
+            <div class="skills">
 
-</div>
+                    <Progress multi>
+                            <Progress bar value="30">JAVASCRIPT</Progress>
+                            <div class="progress-bar">
+                               <span class="progress-value" >75%</span>
+                                    </div>
+                          </Progress><br/>
+
+
+                    <Progress multi>
+                            <Progress bar value="30">HTML</Progress>
+                            <div class="progress-bar">
+                               <span class="progress-value yellow" >85%</span>
+                                    </div>
+                          </Progress><br/>
+
+
+                          <Progress multi>
+                                  <Progress bar value="30">CSS</Progress>
+                                  <div class="progress-bar">
+                                     <span class="progress-value green" >90%</span>
+                                          </div>
+                                </Progress><br/>
+                                <Progress multi>
+                                        <Progress bar value="30">SQL</Progress>
+                                        <div class="progress-bar">
+                                           <span class="progress-value blue" >75%</span>
+                                                </div>
+                                      </Progress><br/>
+
+                                      <Progress multi>
+                                              <Progress bar value="30">REACT</Progress>
+                                              <div class="progress-bar">
+                                                 <span class="progress-value lgreen" >70%</span>
+                                                      </div>
+                                            </Progress><br/>
+                                            <Progress multi>
+                                                    <Progress bar value="30">DJANGO</Progress>
+                                                    <div class="progress-bar">
+                                                       <span class="progress-value dgreen" >70%</span>
+                                                            </div>
+                                                  </Progress><br/>
+                                                  <Progress multi>
+                                                          <Progress bar value="30">FLASK</Progress>
+                                                          <div class="progress-bar">
+                                                             <span class="progress-value dred" >65%</span>
+                                                                  </div>
+                                                        </Progress><br/>
+                                                        <Progress multi>
+                                                                <Progress bar value="30">R</Progress>
+                                                                <div class="progress-bar">
+                                                                   <span class="progress-value dblue" >80%</span>
+                                                                        </div>
+                                                              </Progress><br/>
+                                                              <Progress multi>
+                                                                      <Progress bar value="30">PYTHON</Progress>
+                                                                      <div class="progress-bar">
+                                                                         <span class="progress-value yb" >85%</span>
+                                                                              </div>
+                                                                    </Progress><br/>
+                                                                    <Progress multi>
+                                                                            <Progress bar value="30">JAVA</Progress>
+                                                                            <div class="progress-bar">
+                                                                               <span class="progress-value red" >80%</span>
+                                                                                    </div>
+                                                                          </Progress><br/>
+                                                                          <Progress multi>
+                                                                                  <Progress bar value="30">MACHINE LEARNING</Progress>
+                                                                                  <div class="progress-bar">
+                                                                                     <span class="progress-value lblack" >80%</span>
+                                                                                          </div>
+                                                                                </Progress><br/>
+                                                                                <Progress multi>
+                                                                                <Progress bar value="30">DEEP LEARNING</Progress>
+                                                                                <div class="progress-bar">
+                                                                                   <span class="progress-value orange" >75%</span>
+                                                                                        </div>
+                                                                              </Progress><br/>
+
+
+
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+            </div>
+          </div>
+
 
 
   </GridWrapper>
